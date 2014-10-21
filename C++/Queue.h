007236a5a38d374590fb51075316f0f3ea451d71
David Arvelo <david@davidarvelo.com>
2014-10-20 23:57:35 -0400

    Initial implementation of a Queue in C++

---
+++ C++/Queue.h
@@ -0,0 +1,29 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "List.h"
+
+using std::move;
+
+template <typename Object>
+class Queue {
+public:
+    void enqueue(const Object& element) {
+        lst.push_front(element);
+    }
+
+    void enqueue(const Object&& element) {
+        lst.push_front(std::move(element));
+    }
+
+    Object dequeue() {
+        Object element = lst.back();
+        lst.pop_back();
+        return element;
+    }
+
+private:
+    List<Object> lst;
+};
+
+#endif
