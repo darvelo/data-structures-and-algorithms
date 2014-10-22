16f7338f98e45ab7d482c6ec1ce995c9b7109d4a
David Arvelo <david@davidarvelo.com>
2014-10-21 22:07:59 -0400

    Add tests for JavaScript Binary Search

---
+++ JavaScript/test/BinarySearch.spec.js
@@ -0,0 +1,44 @@
+var chai = require('chai');
+var binarySearch = require('../BinarySearch');
+
+chai.should();
+
+describe('Binary Search', function () {
+  var arr;
+
+  beforeEach(function () {
+    arr = [1,2,3,4,5,6,7,8,9];
+  });
+
+  it('finds an item at index 0', function () {
+    binarySearch(arr, 1).should.equal(0);
+  });
+
+  it('finds an item at the last index', function () {
+    binarySearch(arr, 9).should.equal(8);
+  });
+
+  it('finds an item in the middle', function () {
+    binarySearch(arr, 5).should.equal(4);
+  });
+
+  it('finds an item between the beginning and the middle', function () {
+    binarySearch(arr, 2).should.equal(1);
+    binarySearch(arr, 3).should.equal(2);
+    binarySearch(arr, 4).should.equal(3);
+  });
+
+  it('finds an item between the end and the middle', function () {
+    binarySearch(arr, 6).should.equal(5);
+    binarySearch(arr, 7).should.equal(6);
+    binarySearch(arr, 8).should.equal(7);
+  });
+
+  it('fails to find an item less than the lowest value', function () {
+    binarySearch(arr, 0).should.equal(-1);
+  });
+
+  it('fails to find an item greater than the highest value', function () {
+    binarySearch(arr, 11).should.equal(-1);
+  });
+});
