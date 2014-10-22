7475da7966bbd9a2d37213ab7e472b26cc1dfabf
David Arvelo <david@davidarvelo.com>
2014-10-21 21:44:20 -0400

    Add BinarySearch implementation in C++

---
+++ C++/BinarySearch.h
@@ -0,0 +1,27 @@
+#ifndef BINARYSEARCH_H
+#define BINARYSEARCH_H
+
+#include <vector>
+using std::vector;
+
+template <typename Comparable>
+int binarySearch(const vector<Comparable>& a, const Comparable& x) {
+    int low  = 0,
+        high = a.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (x < a[mid]) {
+            high = mid - 1;
+        } else if (x > a[mid]) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return -1;
+}
+
+#endif
