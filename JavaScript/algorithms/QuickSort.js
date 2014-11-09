50becb918450f2dd05c9b5e7cf4f27748f77cddd
David Arvelo <david@davidarvelo.com>
2014-11-08 23:59:46 -0500

    Add implementation of entropy-optimal quicksort in JavaScript

---
+++ JavaScript/algorithms/QuickSort.js
@@ -0,0 +1,76 @@
+// This is an example of entropy-optimal sorting, where arrays with
+// large numbers of duplicate keys can be efficiently sorted. The
+// trick is to partition the subarrays into three, in the order of:
+//
+//   * those values less than the pivot
+//   * those values equal to the pivot
+//   * those values greater than the pivot
+//
+// This method is great if you know your array has a large number of
+// duplicate keys, but is less performant than standard quicksort if not,
+// since there are many more swaps for non-duplicates here by comparison.
+
+function swap (array, i, j) {
+  var tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+function median3 (array, left, right) {
+  var mid = left + ((right - left) >> 1);
+
+  if (array[mid] < array[left]) {
+    swap(array, left, mid);
+  }
+
+  if (array[right] < array[left]) {
+    swap(array, left, right);
+  }
+
+  if (array[right] < array[mid]) {
+    swap(array, mid, right);
+  }
+
+  // place pivot where it can be moved as the algorithm progresses
+  swap(array, mid, left);
+  return array[left];
+}
+
+function defaultCompare (a, b) {
+  return (a < b) ? -1 : (b < a) ? 1 : 0;
+}
+
+function quickSort (array, left, right, cmp) {
+  if (right <= left) {
+    return;
+  }
+
+  cmp = (typeof cmp === 'function') ? cmp : defaultCompare;
+
+  var pivot = median3(array, left, right),
+      i  = left + 1,
+      lt = left,
+      gt = right,
+      result;
+
+  while (i <= gt) {
+    result = cmp(array[i], pivot);
+
+    if (result < 0) {
+      swap(array, lt++, i++);
+    } else if (result > 0) {
+      swap(array, i, gt--);
+    } else {
+      ++i;
+    }
+  }
+
+  quickSort(array, left, lt - 1, cmp);
+  quickSort(array, gt + 1, right, cmp);
+}
+
+function sort (array, cmp) {
+  quickSort(array, 0, array.length - 1, cmp);
+}
+
+module.exports = sort;
---
+++ JavaScript/test/QuickSort.spec.js
@@ -0,0 +1,40 @@
+var prototest = require('./prototest');
+var quickSort = require('../algorithms/QuickSort.js');
+
+prototest({
+  title: 'QuickSort',
+  sortFunc: quickSort,
+  sortFuncIterators: null,
+  testArrays: true,
+  testIterators: false
+});
+
+describe('QuickSort with Custom Compare Function', function () {
+  it('should sort in reverse order', function () {
+    var array = [4,1,6,3,7,9,8,2,5];
+    var cmp = function (a, b) {
+      return b - a;
+    };
+
+    quickSort(array , cmp);
+
+    array.should.deep.equal([9,8,7,6,5,4,3,2,1]);
+  });
+
+  it('should sort objects by a key', function () {
+    var array = [{ key: 'dude' }, { key: 'wheres' }, { key: 'my' }, { key: 'car' }];
+    var cmp = function (a, b) {
+      return a.key.length - b.key.length;
+    };
+
+    quickSort(array, cmp);
+
+    array.should.deep.equal([{ key: 'my' }, { key: 'car' }, { key: 'dude' }, { key: 'wheres' }]);
+  });
+
+  it('should sort with many duplicate keys', function () {
+    var array = [1,4,3,2,5,3,7,6,7,4,4,4,4,7,6,3,3,3,3,3,1,2,2];
+    quickSort(array);
+    array.should.deep.equal([1,1,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,5,6,6,7,7,7]);
+  });
+});
