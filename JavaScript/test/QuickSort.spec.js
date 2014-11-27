da7a41cc2d81be453ca4238713ed6195aac0b34d
David Arvelo <david@davidarvelo.com>
2014-11-26 23:16:49 -0500

    leverage additionalTests function

--- JavaScript/test/QuickSort.spec.js
+++ JavaScript/test/QuickSort.spec.js
@@ -6,35 +6,38 @@ testFactory({
   sortFunc: quickSort,
   sortFuncIterators: null,
   testArrays: true,
-  testIterators: false
+  testIterators: false,
+  additionalTests: additionalTests,
 });
 
-describe('QuickSort with Custom Compare Function', function () {
-  it('should sort in reverse order', function () {
-    var array = [4,1,6,3,7,9,8,2,5];
-    var cmp = function (a, b) {
-      return b - a;
-    };
+function additionalTests () {
+  describe('custom compare function', function () {
+    it('should sort in reverse order', function () {
+      var array = [4,1,6,3,7,9,8,2,5];
+      var cmp = function (a, b) {
+        return b - a;
+      };
 
-    quickSort(array, { compareFunc: cmp });
+      quickSort(array, { compareFunc: cmp });
 
-    array.should.deep.equal([9,8,7,6,5,4,3,2,1]);
+      array.should.deep.equal([9,8,7,6,5,4,3,2,1]);
+    });
+
+    it('should sort objects by a key', function () {
+      var array = [{ key: 'dude' }, { key: 'wheres' }, { key: 'my' }, { key: 'car' }];
+      var cmp = function (a, b) {
+        return a.key.length - b.key.length;
+      };
+
+      quickSort(array, { compareFunc: cmp });
+
+      array.should.deep.equal([{ key: 'my' }, { key: 'car' }, { key: 'dude' }, { key: 'wheres' }]);
+    });
+
+    it('should sort with many duplicate keys', function () {
+      var array = [1,4,3,2,5,3,7,6,7,4,4,4,4,7,6,3,3,3,3,3,1,2,2];
+      quickSort(array);
+      array.should.deep.equal([1,1,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,5,6,6,7,7,7]);
+    });
   });
-
-  it('should sort objects by a key', function () {
-    var array = [{ key: 'dude' }, { key: 'wheres' }, { key: 'my' }, { key: 'car' }];
-    var cmp = function (a, b) {
-      return a.key.length - b.key.length;
-    };
-
-    quickSort(array, { compareFunc: cmp });
-
-    array.should.deep.equal([{ key: 'my' }, { key: 'car' }, { key: 'dude' }, { key: 'wheres' }]);
-  });
-
-  it('should sort with many duplicate keys', function () {
-    var array = [1,4,3,2,5,3,7,6,7,4,4,4,4,7,6,3,3,3,3,3,1,2,2];
-    quickSort(array);
-    array.should.deep.equal([1,1,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,5,6,6,7,7,7]);
-  });
-});
+}
