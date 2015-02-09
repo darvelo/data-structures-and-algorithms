c6aa18c4dd327553d00da598517dfaeb68391437
David Arvelo <david@davidarvelo.com>
2015-02-09 01:05:30 -0500

    Reorganize C++ bottom-up mergesort

--- C++/algorithms/MergeSortBottomUp.h
+++ C++/algorithms/MergeSortBottomUp.h
@@ -8,8 +8,7 @@
  * Function Prototypes *
  ***********************/
 template <typename Comparable>
-void sort (std::vector<Comparable>* a, std::vector<Comparable>* b, size_t low, size_t high);
-
+void mergeSort_bottomup (std::vector<Comparable>& vec);
 template <typename Comparable>
 void merge_bottomup (std::vector<Comparable>& a, std::vector<Comparable>& b, size_t left, size_t middle, size_t right);
 
@@ -17,23 +16,21 @@ void merge_bottomup (std::vector<Comparable>& a, std::vector<Comparable>& b, siz
  * Implementation *
  ******************/
 template <typename Comparable>
-void mergeSort_bottomup (std::vector<Comparable>& a) {
-    std::vector<Comparable> scratch(a.size());
-    sort(&a, &scratch, 0, a.size());
-}
+void mergeSort_bottomup (std::vector<Comparable>& vec) {
+    std::vector<Comparable> scratch(vec.size());
 
-template <typename Comparable>
-void sort (std::vector<Comparable>* a, std::vector<Comparable>* b, size_t low, size_t high)
-{
-    std::vector<Comparable>* original = a;
-    std::vector<Comparable>* scratch = b;
+    std::vector<Comparable>* original = &vec;
+    std::vector<Comparable>* a = &vec;
+    std::vector<Comparable>* b = &scratch;
     std::vector<Comparable>* tmp;
 
-    for (size_t p = 1; p < high; p <<= 1) {
-        for (size_t i = 0; i < high; i += p << 1) {
+    size_t size = vec.size();
+
+    for (size_t p = 1; p < size; p <<= 1) {
+        for (size_t i = 0; i < size; i += p << 1) {
             size_t left = i;
-            size_t middle = (i + p) > high ? high : i + p;
-            size_t right = (i + (p << 1)) > high ? high : i + (p << 1);
+            size_t middle = (i + p) > size ? size : i + p;
+            size_t right = (i + (p << 1)) > size ? size : i + (p << 1);
             merge_bottomup(*a, *b, left, middle, right);
         }
 
@@ -43,7 +40,7 @@ void sort (std::vector<Comparable>* a, std::vector<Comparable>* b, size_t low, s
     }
 
     if (original != a) {
-        for (size_t i = 0; i < high; ++i) {
+        for (size_t i = 0; i < size; ++i) {
             (*original)[i] = (*a)[i];
         }
     }
