85e95bdbc07fe3e1be85985a19e18845457ef09c
David Arvelo <david@davidarvelo.com>
2015-04-01 21:04:49 -0400

    C++: Start working on Graph implementation

---
+++ C++/data-structures/Graph.h
@@ -0,0 +1,119 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <list>
+#include <iostream>
+#include <unordered_map>
+
+template <typename Data>
+class Graph {
+private:
+    class Vertex {
+    public:
+        explicit Vertex(std::string _name, int _weight = 0, Data* _data = nullptr)
+            : name(_name), weight(_weight), data(_data) { }
+
+        void print(std::ostream& out = std::cout) {
+            out << "Name: " << name << ", Weight: " << weight << ", Edges: ";
+            for (auto& v : edges) {
+                out << v->name;
+            }
+            out << std::endl;
+        }
+
+        int weight = 0;
+        const std::string name;
+        // pointer itself should stay const
+        const Data* data = nullptr;
+    private:
+        std::list<Vertex*> edges;
+        friend Graph<Data>;
+    };
+public:
+    class iterator {
+    public:
+        Vertex& operator*() {
+            return *current->second;
+        }
+
+        iterator& operator++() {
+            ++current;
+            return *this;
+        }
+
+        iterator& operator++(int) {
+            iterator old = *this;
+            ++(*this);
+            return old;
+        }
+
+        iterator& operator--() {
+            --current;
+            return *this;
+        }
+
+        iterator operator--(int) {
+            iterator old = *this;
+            --(*this);
+            return old;
+        }
+
+        bool operator==(const iterator& rhs) {
+            return current == rhs.current;
+        }
+
+        bool operator!=(const iterator& rhs) {
+            return !(current = rhs.current);
+        }
+    private:
+        iterator(typename std::unordered_map<std::string,Vertex*>::iterator itr)
+            : current(itr) { }
+
+        typename std::unordered_map<std::string, Vertex*>::iterator current;
+        friend Graph<Data>;
+    };
+public:
+    Graph() { }
+
+    Vertex& addVertex(std::string name, int weight = 0, Data* data = nullptr) {
+        Vertex* v = new Vertex{name, weight, data};
+        std::pair<std::string, Vertex*> item{name, v};
+
+        auto result = vertices.insert(item);
+
+        // if insert failed, map already had such a key.
+        // grab the element associated with that key
+        if (!result.second) {
+            delete v;
+            v = (*result.first).second;
+        }
+
+        return *v;
+    }
+
+    iterator getVertex(std::string name) {
+        auto iter = vertices.find(name);
+
+        if (iter == vertices.end()) {
+            return end();
+        }
+
+        return iterator(iter);
+    }
+
+    void print(std::ostream& out = std::cout) {
+        out << "Size: " << size() << std::endl;
+        for (auto& itr : vertices) {
+            out << itr.second->name;
+        }
+        out << std::endl;
+    }
+
+    size_t size()    { return vertices.size(); }
+    iterator begin() { return iterator(vertices.begin()); }
+    iterator end()   { return iterator(vertices.end());   }
+private:
+    std::unordered_map<std::string, Vertex*> vertices;
+};
+
+#endif
