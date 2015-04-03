63d96264f25f8bcb3c60d64731466983d12a40a0
David Arvelo <david@davidarvelo.com>
2015-04-02 21:55:07 -0400

    C++: Add utils.h for CustomException class

---
+++ C++/utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <exception>
+
+class CustomException : public std::exception {
+public:
+    CustomException(const char* _message) : message(_message) { }
+    ~CustomException() throw() { }
+    virtual const char* what() const throw() {
+        return message;
+    };
+private:
+    const char* message;
+};
+
+#endif
