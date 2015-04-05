6a5d96792c0a1366330b9231c5d37d61174b3557
David Arvelo <david@davidarvelo.com>
2015-04-04 23:03:48 -0400

    testBfs gets all vertices' distances from root

--- C++/algorithms/Graphs/testBfs.cpp
+++ C++/algorithms/Graphs/testBfs.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include <map>
 #include <assert.h>
 #include "../../data-structures/Graph.h"
 #include "./testDefs.h"
@@ -9,7 +9,14 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-using std::stack;
+
+struct mapCompare {
+    bool operator() (typename Graph<Data>::Vertex* v, typename Graph<Data>::Vertex* w) {
+        return v->name < w->name;
+    }
+};
+
+std::map<typename Graph<Data>::Vertex*, int, mapCompare> distances;
 
 void processEarly(typename Graph<Data>::Vertex& v) {
     cout << v.name << " was processed early" << endl;
@@ -17,6 +24,12 @@ void processEarly(typename Graph<Data>::Vertex& v) {
 
 void processLate(typename Graph<Data>::Vertex& v) {
     cout << v.name << " was processed late" << endl;
+
+    if (!v.parent) {
+        distances.insert({ &v, 0 });
+    } else {
+        distances.insert({ &v, distances[v.parent]+1 });
+    }
 }
 
 void processEdge(typename Graph<Data>::Vertex& v, typename Graph<Data>::Vertex& w) {
@@ -24,12 +37,14 @@ void processEdge(typename Graph<Data>::Vertex& v, typename Graph<Data>::Vertex& 
 }
 
 void shortestPath(typename Graph<Data>::Vertex* start, typename Graph<Data>::Vertex* end, string& path) {
-    if (!start) {
+    // catch when end isn't a descendent of start,
+    // or climbing the tree goes too far (past start)
+    if (!end || start->parent == end) {
         return;
     }
 
-    shortestPath(start->parent, end, path);
-    path += start->name + " ";
+    shortestPath(start, end->parent, path);
+    path += " " + end->name;
 }
 
 int main() {
@@ -41,12 +56,16 @@ int main() {
     g.print();
 
     string path;
-    auto start = &*g.getVertex("v6");
-    auto end = &*g.getVertex("v1");
+    auto start = &*g.getVertex("v1");
+    auto end = &*g.getVertex("v6");
+
+    for (auto& pr : distances) {
+        cout << pr.first->name << " distance from v1: " << pr.second << endl;
+    }
 
     shortestPath(start, end, path);
-    assert(path == "v1 v4 v6 ");
-    cout << path << endl;
+    assert(path == " v1 v4 v6");
+    cout << "Shortest path from v1 to v6:" << path << endl;
 
     return 0;
 }
