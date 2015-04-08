60ae97366c1a4413ea071c9e30d6c40de1adae9f
David Arvelo <david@davidarvelo.com>
2015-04-08 17:17:29 -0400

    C++ Graph: Make topsort more object-oriented

--- C++/algorithms/Graphs/topsort.cpp
+++ C++/algorithms/Graphs/topsort.cpp
@@ -9,37 +9,44 @@ using std::endl;
 using std::stack;
 using std::bind;
 using std::ref;
-using std::placeholders::_1;
-using std::placeholders::_2;
+using namespace std::placeholders;
 
 typedef Graph<Data>::Vertex Vertex;
 
-void processLate (stack<Vertex*>& sorted, Vertex& v) {
-    sorted.push(&v);
-}
+struct TopSortResult {
+    TopSortResult(Graph<Data>& _g) : g(_g) { };
 
-void processEdge (Graph<Data>& g, Vertex& v, Vertex& w) {
-    auto type = g.edgeClassification(v, w);
-
-    if (type == g.BACK_EDGE) {
-        cout << "Warning: directed cycle found, not a DAG." << endl;
-        g.finished = true;
+    void processVertexLate (Vertex& v) {
+        result.push(&v);
     }
-}
+
+    void processEdge (Vertex& v, Vertex& w) {
+        auto type = g.edgeClassification(v, w);
+
+        if (type == g.BACK_EDGE) {
+            cout << "Warning: directed cycle found, not a DAG." << endl;
+            g.finished = true;
+        }
+    }
+
+    stack<Vertex*> result;
+    Graph<Data>& g;
+};
 
 int main() {
     bool directed = true;
     Graph<Data> g(directed);
-    stack<Vertex*> sorted;
-
-    readIntoGraph(g, "input/topsort_graph.txt");
-
-    auto pLate = bind(processLate, ref(sorted), _1);
-    auto pEdge = bind(processEdge, ref(g), _1, _2);
 
     if (!g.directed) {
         throw CustomException("Can't do topological sort on an undirected graph!");
     }
+
+    readIntoGraph(g, "input/topsort_graph.txt");
+
+    TopSortResult t(g);
+    auto& result = t.result;
+    auto pLate = bind(&TopSortResult::processVertexLate, ref(t), _1);
+    auto pEdge = bind(&TopSortResult::processEdge, ref(t), _1, _2);
 
     for (auto& v : g) {
         if (!v.discovered) {
@@ -49,16 +56,17 @@ int main() {
 
     cout << "Topological Sort: ";
 
-    if (sorted.size()) {
-        cout << sorted.top()->name;
-        sorted.pop();
-    } else {
-        cout << "failed.";
+    if (g.finished || result.empty()) {
+        cout << "failed." << endl;
+        return 1;
     }
 
-    while (!sorted.empty()) {
-        cout << " -> " << sorted.top()->name;
-        sorted.pop();
+    cout << result.top()->name;
+    result.pop();
+
+    while (!result.empty()) {
+        cout << " -> " << result.top()->name;
+        result.pop();
     }
 
     cout << endl;
