93234be1b4a66333cb5a17ff0d9dcc17905f9347
David Arvelo <david@davidarvelo.com>
2015-04-10 23:23:56 -0400

    C++ Graph: More robust instantiation via inputfile
    Code broken up into routines. Helpful error messages. More docs.
    Overall better.

--- C++/algorithms/Graphs/helpers.h
+++ C++/algorithms/Graphs/helpers.h
@@ -6,10 +6,12 @@
 #include <fstream>
 #include <sstream>
 #include <regex>
+#include <exception>
 #include "../../data-structures/Graph.h"
 
 struct Data {
-    Data(std::string _surname = "") : surname(_surname) { }
+    Data(std::string _surname = "")
+        : surname(_surname) { }
     std::string surname;
 };
 
@@ -18,20 +20,63 @@ struct VertexRelationship {
     std::vector<std::string> edges;
 };
 
-// format: name weight edges...
+class BadVertexException : public std::exception {
+public:
+    BadVertexException(std::string _name)
+        : name(_name) { }
+    ~BadVertexException() throw() { }
+    virtual const char* what() const throw() {
+        return ("Accessing vertex: \"" + name + "\" failed!").c_str();
+    }
+
+    std::string name;
+};
+
+class BadEdgeException : public std::exception {
+public:
+    BadEdgeException(std::string _source, std::string _sink)
+        : source(_source), sink(_sink) { }
+    ~BadEdgeException() throw() { }
+    virtual const char* what() const throw() {
+        return ("Adding edge: \"" + source + " -> " + sink + "\" failed!").c_str();
+    }
+
+    std::string source;
+    std::string sink;
+};
+
+// format for lines in the inputfile:
+//
+//     vertexName vertexWeight edge...
+//
+// vertexName   => string
+// vertexWeight => int
+// edge         => vertexName
+//
+// example:
+//     v1 10 v2
+//     v2 1 v3
+//     v3 2
+//
+// creates three vertices: v1, v2, and v3. v1 points to v2 (v2 also points to v1 in
+// an undirected graph), v2 points to v3, and v3 doesn't point to anything.
+//
+// NOTE: naming an edge vertex does not implicitly create that vertex in the graph.
+//       an error will be thrown in this case. only vertices specified as the first
+//       word on a line will be created.
 static std::string vertex_string = "^([a-zA-Z0-9]+)\\s+([0-9]+)(?:\\s+([\\sa-zA-Z0-9]+))?\\s*$";
 static std::regex vertex_regex(vertex_string);
 
 template <typename Data>
+static
 void
-readIntoGraph(Graph<Data>& g, std::string filename) {
-    std::ifstream f(filename);
+addVertices(Graph<Data>& g, std::string filename, std::vector<VertexRelationship>& vertices) {
+    std::ifstream file(filename);
     std::string line;
     std::smatch match;
-    std::vector<VertexRelationship> vertices;
     std::vector<std::string> empty;
 
-    while (std::getline(f, line)) {
+    while (std::getline(file, line)) {
         if (line.empty() || line.size() == 1) {
             continue;
         }
@@ -50,27 +95,37 @@ readIntoGraph(Graph<Data>& g, std::string filename) {
         }
     }
 
-    auto begin = vertices.begin();
-    auto end = vertices.end();
+    file.close();
+}
 
-    while (begin != end) {
-        VertexRelationship& r = *begin;
-        auto itr = g.getVertex(r.name);
+template <typename Data>
+void
+addEdges(Graph<Data>& g, std::vector<VertexRelationship>& vertices) {
+    for (VertexRelationship& r : vertices) {
+        auto& source = r.name;
 
-        if (itr != g.end()) {
-            auto v = *itr;
-            auto edgesBegin = r.edges.begin();
-            auto edgesEnd = r.edges.end();
-
-            while (edgesBegin != edgesEnd) {
-                g.addEdge(v.name, *edgesBegin++);
-            }
+        if (g.getVertex(source) == g.end()) {
+            throw BadVertexException(source);
         }
 
-        ++begin;
-    }
+        for (auto& sink : r.edges) {
+            if (g.getVertex(sink) == g.end()) {
+                throw BadVertexException(sink);
+            }
 
-    f.close();
+            if(!g.addEdge(source, sink)) {
+                throw BadEdgeException(source, sink);
+            }
+        }
+    }
+}
+
+template <typename Data>
+void
+readIntoGraph(Graph<Data>& g, std::string filename) {
+    std::vector<VertexRelationship> vertices;
+    addVertices(g, filename, vertices);
+    addEdges(g, vertices);
 }
 
 #endif
