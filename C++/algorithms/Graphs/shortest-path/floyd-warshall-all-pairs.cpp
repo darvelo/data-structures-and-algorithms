0d86f68c5b15ad268fdf0ec6658f6ccdef568c06
David Arvelo <david@davidarvelo.com>
2015-04-17 00:47:42 -0400

    C++ Graph: Simplify All-Pairs path traversal
    A small change in the way the path matrix is generated shaves off 50
    lines of recursive code used to traverse and store it into a vector.

--- C++/algorithms/Graphs/shortest-path/floyd-warshall-all-pairs.cpp
+++ C++/algorithms/Graphs/shortest-path/floyd-warshall-all-pairs.cpp
@@ -19,15 +19,16 @@ static int MAX_INT = std::numeric_limits<int>::max();
 
 void allPairs(const Matrix<int>& adj,
                     Matrix<int>& dist,
-                    Matrix<int>& path)
+                    Matrix<int>& next)
 {
     int n = adj.nrows();
+    int distance;
 
     // initialize dist and path
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            dist[i][j] = adj[i][j];
-            path[i][j] = -1;
+            distance = dist[i][j] = adj[i][j];
+            next[i][j] = (distance == MAX_INT) ? -1 : j;
         }
     }
 
@@ -42,13 +43,14 @@ void allPairs(const Matrix<int>& adj,
                 // skip if no path from intermediate vertex to destination
                 if (dist[k][j] == MAX_INT) continue;
 
+                // we're assured we're not adding MAX_INT and overflowing here
                 int through_k = dist[i][k] + dist[k][j];
 
                 // if the path directly from i to j is longer than through k
                 if (dist[i][j] > through_k) {
                     // update shortest path
                     dist[i][j] = through_k;
-                    path[i][j] = k;
+                    next[i][j] = next[i][k];
                 }
             }
         }
@@ -71,79 +73,25 @@ vertexNameToInt(GraphT& g,
 }
 
 template <typename Vertex>
-void
-backward(Matrix<int>& path,
-         vector<string>& vec,
-         int i,
-         int j,
-         unordered_map<int, Vertex*>& itov)
-{
-    // even though the path is -1 (no intermediate vertex), we know i and j are directly connected,
-    // since we know j is an intermediate vertex between our starting vertex i and our destination
-    if (path[i][j] == -1) {
-        vec.push_back(itov[i]->name);
-        return;
-    }
-    // get to the first intermediate vertex after i between i and k
-    backward(path, vec, i, path[i][j], itov);
-    // push all intermediate vertices between i and k in order
-    vec.push_back(itov[path[i][j]]->name);
-}
-
-template <typename Vertex>
-void
-forward(Matrix<int>& path,
-        vector<string>& vec,
-        int i,
-        int j,
-        unordered_map<int, Vertex*>& itov)
-{
-    // even though the path is -1 (no intermediate vertex), we know i and j are directly connected,
-    // since we know i is an intermediate vertex between our starting vertex and our destination, j
-    if (path[i][j] == -1) {
-        vec.push_back(itov[j]->name);
-        return;
-    }
-    // push all intermediate vertices between k and j in order
-    vec.push_back(itov[path[i][j]]->name);
-    // get to the first intermediate vertex before j between k and j
-    forward(path, vec, path[i][j], j,itov);
-}
-
-template <typename Vertex>
-void
-recurse(Matrix<int>& path,
-        vector<string>& vec,
-        int i,
-        int j,
-        unordered_map<int, Vertex*>& itov)
-{
-    int k = path[i][j];
-    // push all intermediate vertices between i and k
-    backward(path, vec, i, k, itov);
-    // push k
-    vec.push_back(itov[k]->name);
-    // push all intermediate vertices between k and j
-    forward(path, vec, k, j, itov);
-}
-
-template <typename Vertex>
 vector<string>
-getPath(Matrix<int>& path,
-        Matrix<int>& dist,
+getPath(Matrix<int>& next,
+        string start,
         int i,
         int j,
         unordered_map<int, Vertex*>& itov)
 {
     vector<string>result;
 
-    if (dist[i][j] == MAX_INT) {
+    if (next[i][j] == -1) {
         result.push_back("NO PATH");
-    } else if (path[i][j] == -1) {
+        return result;
+    }
+
+    result.push_back(start);
+
+    while (i != j) {
+        i = next[i][j];
         result.push_back(itov[i]->name);
-        result.push_back(itov[j]->name);
-    } else {
-        recurse(path, result, i, j, itov);
     }
 
     return result;
@@ -165,32 +113,32 @@ int main(int argc, char* argv[]) {
 
     int n = g.nvertices();
     Matrix<int> dist(n, n);
-    Matrix<int> path(n, n);
+    Matrix<int> next(n, n);
 
     for (auto pr : itov) {
         cout << pr.first << ": " << pr.second->name << endl;
     }
     cout << endl;
 
-    allPairs(adj, dist, path);
+    allPairs(adj, dist, next);
     cout << "Adjacency Matrix:" << endl << adj;
     cout << "Distance Matrix:"  << endl << dist;
-    cout << "Path Matrix:"      << endl << path;
+    cout << "Path Matrix:"      << endl << next;
 
     // print path between labeled vertices in arguments
     if (argc == 3) {
         int i = vertexNameToInt(g, argv[1], vtoi);
         int j = vertexNameToInt(g, argv[2], vtoi);
-        auto vec = getPath(path, dist, i, j, itov);
+        vector<string> path = getPath(next, argv[1], i, j, itov);
 
-        if (!vec.empty()) {
-            cout << vec[0];
+        if (!path.empty()) {
+            cout << path[0];
         }
-        for (decltype(vec.size()) i = 1; i < vec.size(); ++i) {
+        for (decltype(path.size()) i = 1; i < path.size(); ++i) {
             cout << " -> ";
-            cout << vec[i];
+            cout << path[i];
         }
-        cout << " *** WEIGHT: " << dist[i][j] << endl;
+        cout << " *** DISTANCE: " << dist[i][j] << endl;
     }
 
     return 0;
